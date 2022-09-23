f88fb88d0c40cecf8f5e452ad4c053a63d24467a
EZEN-133 <EZEN-133@DESKTOP-CVEHFNM>
2022-09-23 19:17:13 +0900

    팀원: 이동현
    한일 : 멤버dto 수정
    
    앞으로 할일: 게시판

--- team3Project/src/main/java/spms/dto/MemberDto.java
+++ team3Project/src/main/java/spms/dto/MemberDto.java
@@ -2,4 +2,102 @@ package spms.dto;
 
 public class MemberDto {
 
+	private int mno = 0;
+	private String id = "";
+	private String email = "";
+	private String pwd = "";
+	private String mname = "";
+	private String nickname = "";
+	private String cre_date = "";
+	private String mod_date = "";
+	
+	public MemberDto() {
+		super();
+	}
+
+	public MemberDto(int mno, String id, String email, String pwd, String mname, String nickname, String cre_date,
+			String mod_date) {
+		super();
+		this.mno = mno;
+		this.id = id;
+		this.email = email;
+		this.pwd = pwd;
+		this.mname = mname;
+		this.nickname = nickname;
+		this.cre_date = cre_date;
+		this.mod_date = mod_date;
+	}
+
+	public int getMno() {
+		return mno;
+	}
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMname() {
+		return mname;
+	}
+
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getCre_date() {
+		return cre_date;
+	}
+
+	public void setCre_date(String cre_date) {
+		this.cre_date = cre_date;
+	}
+
+	public String getMod_date() {
+		return mod_date;
+	}
+
+	public void setMod_date(String mod_date) {
+		this.mod_date = mod_date;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDto [mno=" + mno + ", id=" + id + ", email=" + email + ", pwd=" + pwd + ", mname=" + mname
+				+ ", nickname=" + nickname + ", cre_date=" + cre_date + ", mod_date=" + mod_date + "]";
+	}
+	
+	
+	
 }
