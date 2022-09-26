c4ee57428d52416d9f9b3e61572609f363a195cc
EZEN-135 <EZEN-135@DESKTOP-EFOC4DA>
2022-09-26 15:47:51 +0900

    손다예 한 일: 회원가입 데이터 db연동
    다음할일: list update delete servlet작성

--- team3Project/src/main/java/spms/dao/MemberDao.java
+++ team3Project/src/main/java/spms/dao/MemberDao.java
@@ -1,5 +1,65 @@
 package spms.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import spms.dto.MemberDto;
+
 public class MemberDao {
 
-}
+	private Connection connection = null;
+	
+	public void setConnection(Connection conn) {
+		this.connection = conn;
+	}
+	
+	public int memberAdd(MemberDto memberDto)
+			throws Exception{
+		
+		PreparedStatement pstmt = null;
+		int rs = 0;
+		
+		try {
+			String id = memberDto.getId();
+			String email = memberDto.getEmail();
+			String pwd = memberDto.getPwd();
+			String name = memberDto.getMname();
+			String nickname = memberDto.getNickname();
+			
+			String sql = "";
+			
+			sql += "INSERT INTO MEMBERS";
+			sql += " VALUE(MNO, ID, EMAIL, PWD, MNAME, NICKNAME, CRE_DATE, MOD_DATE)";
+			sql += " VALUES(MEMBERS_MNO_SEQ.NEXTVAL, ?"; 
+			sql += " , ?, ?, ?, ?, SYSDATE, SYSDATE)";
+		
+			pstmt = connection.prepareStatement(sql);
+			
+			pstmt.setString(1, id);
+			pstmt.setString(2, email);
+			pstmt.setString(3, pwd);
+			pstmt.setString(4, name);
+			pstmt.setString(5, nickname);
+			
+			rs = pstmt.executeUpdate();
+		}catch (Exception e) {
+			throw e;
+		}finally {
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}//finally 끝
+		
+		return rs;
+	}//memberInsert 끝
+	
+	
+	
+	
+}//memberDao 끝
